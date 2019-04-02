cf2d88b3763114a50b5ed0e0831061a8569b30a6
Максим <kyngfy-panda@yandex.ru>
2019-04-02 14:51:43 +0500

    Добавил интерфейс пользователя

---
+++ DietMenu2/Models/IUser.cs
@@ -0,0 +1,40 @@
+﻿
+namespace DietMenu2.Models
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+
+        string Login { get; set; }
+
+        string Hash { get; set; }
+
+        string Password { get; set; }
+
+        string Name { get; set; }
+
+        string Surname { get; set; }
+
+        string Weight { get; set; }
+
+        string Height { get; set; }
+
+        string Age { get; set; }
+
+        string GenderId { get; set; }
+
+        double GetUnformattedWeight { get; }
+
+        double GetUnformattedHeight { get; }
+
+        int GetUnformattedAge { get; }
+
+        int GetUnformattedGenderId { get; }
+
+        string FullName { get; }
+
+        int TypeId { get; }
+
+        int EntityId { get; }
+    }
+}
