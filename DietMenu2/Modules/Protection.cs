d80c2c9d3ad37a39f4e3ac4d14bd2151d8838f2a
Максим <kyngfy-panda@yandex.ru>
2019-04-02 15:04:49 +0500

    Добавил модуль работы с хэшами

---
+++ DietMenu2/Modules/Protection.cs
@@ -0,0 +1,32 @@
+﻿
+namespace DietMenu2.Modules
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public static class Protection
+    {
+        private static readonly SHA256 Encryptor = SHA256.Create();
+
+        public static byte[] GenerateHash(string login, string password)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(login + password);
+            return Encryptor.ComputeHash(data);
+        }
+
+        public static bool CheckHash(string userLogin, string password, string protectKey)
+        {
+            bool isRight = true;
+            byte[] key = GenerateHash(userLogin, password);
+            for (int i = 0; i < key.Length && isRight; i++)
+            {
+                if (key[i] != protectKey[i])
+                {
+                    isRight = false;
+                }
+            }
+
+            return isRight;
+        }
+    }
+}
